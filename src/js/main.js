2246e0ca0c9fc7815cb3ec8c98ec8d4020a7d250
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-09-22 14:31:34 +0100

    images added, basic html and some classes added

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector("#menu-btn");
+const navLinks = document.querySelector("#navbar-links");
+const menuBtn = document.querySelector("#menu-btn");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener('click', function) {
+    // code
+}
